ceb084b0095f914b96d251f0a9c061bd030e46aa
ajdiazh <adiaz@indimohn.com>
2022-12-22 10:12:42 -0600

    Initial commit

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagement.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.postId = id;
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
